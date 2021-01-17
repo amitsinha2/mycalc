f0ef2b3544f5b83404c5397fecb8fa4e63b8f12f
Ubuntu <amit.sinha2@mindtree.com>
2021-01-17 18:06:53 +0000

    Modified validation details

--- src/main/java/com/calculator/mycalc/controller/CalculatorController.java
+++ src/main/java/com/calculator/mycalc/controller/CalculatorController.java
@@ -10,7 +10,7 @@ public class CalculatorController {
 	@GetMapping("/calculate/{number1}/{number2}/{operation}")
 	public String calculate(@PathVariable("number1") long number1, @PathVariable("number2") long number2,
 			@PathVariable("operation") String operation) throws Exception {
-		if (operation == null || number2 == 0) {
+		if (operation == null || number1 == 0 ||  number2 == 0) {
 			return "Invalid inputs";
 		}
 		if (operation.equalsIgnoreCase("+") || operation.equalsIgnoreCase("add")) {
